aea370049cbcd0e3a63bba2bd5f4877fa15337cb
ArtemShDev <dev.art.balt@gmail.com>
2021-12-26 00:30:47 +0300

    0. HQL [#6874]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -1,6 +1,21 @@
 create table if not exists items (
                                       id serial primary key,
                                       created timestamp,
-                                      description text not null,
-                                      done boolean
+                                      description varchar(500) not null,
+                                      done boolean,
+                                      user_id int references users(id)
+);
+
+create table if not exists users (
+                                     id serial primary key,
+                                     username varchar(100) not null,
+                                     email varchar(100) not null unique,
+                                     password varchar(100) not null
+);
+
+create table if not exists candidates (
+                                     id serial primary key,
+                                     name varchar(100) not null,
+                                     experience varchar(100) not null,
+                                     salary int not null
 );

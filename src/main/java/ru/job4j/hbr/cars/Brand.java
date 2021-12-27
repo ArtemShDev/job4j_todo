0b205cdaaf5216e9e2ed62637d1695412f9b3856
ArtemShDev <dev.art.balt@gmail.com>
2021-12-27 14:37:20 +0300

    1. ToMany [#301848]

---
+++ src/main/java/ru/job4j/hbr/cars/Brand.java
@@ -0,0 +1,69 @@
+package ru.job4j.hbr.cars;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "brands")
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Model> models = new ArrayList<>();
+
+    public Brand() {
+    }
+
+    public Brand(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Model> getModels() {
+        return models;
+    }
+
+    public void setModels(List<Model> models) {
+        this.models = models;
+    }
+
+    public void addModel(Model model) {
+        models.add(model);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Brand brand = (Brand) o;
+        return id == brand.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/ru/job4j/hbr/cars/HbrRun.java
@@ -0,0 +1,41 @@
+package ru.job4j.hbr.cars;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HbrRun {
+
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+            Brand toyota = new Brand("Toyota");
+            session.save(toyota);
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    private static void addModels(Brand brand) {
+        Model corolla = new Model("Corolla");
+        Model camry = new Model("Camry");
+        Model vitz = new Model("Vitz");
+        Model yaris = new Model("Yaris");
+        Model iq = new Model("IQ");
+        brand.addModel(corolla);
+        brand.addModel(vitz);
+        brand.addModel(camry);
+        brand.addModel(yaris);
+        brand.addModel(iq);
+    }
+}

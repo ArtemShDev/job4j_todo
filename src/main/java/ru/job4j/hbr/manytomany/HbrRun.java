c2014f088778e0c7122b59cfbdbcbdc6a87b15d2
ArtemShDev <dev.art.balt@gmail.com>
2021-12-27 18:47:20 +0300

    2. ManyToMany [#331986]

---
+++ src/main/java/ru/job4j/hbr/manytomany/HbrRun.java
@@ -0,0 +1,50 @@
+package ru.job4j.hbr.manytomany;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class HbrRun {
+
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+            List<Author> authors = getAuthors();
+            session.save(authors.get(0));
+            session.save(authors.get(1));
+            session.save(authors.get(2));
+            session.getTransaction().commit();
+            session.beginTransaction();
+            Author author = session.get(Author.class, 14);
+            session.remove(author);
+            session.getTransaction().commit();
+            session.close();
+        }  catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+
+    private static List<Author> getAuthors() {
+        Author author = new Author("Dostoevskiy");
+        author.addBook(new Book("Idiot"));
+        author.addBook(new Book("White Nights"));
+        Author author2 = new Author("Professor Ivanov");
+        Author author3 = new Author("Professor McLaren");
+        Book book = new Book("solid state physics");
+        author2.addBook(book);
+        author2.addBook(new Book("Dynamics"));
+        author3.addBook(book);
+        author3.addBook(new Book("Kinematics"));
+        return List.of(author, author2, author3);
+    }
+}
